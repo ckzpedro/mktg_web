3fe6981a8dc74ffb55cfa52a8b3044b370b8d227
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2024-04-06 20:45:40 -0300

    feat: Adjust cardList and create new styles

--- src/components/AddedItens.tsx
+++ src/components/AddedItens.tsx
@@ -11,19 +11,19 @@ export default function AddedItens() {
       title: 'Item 2',
       quantity: '2',
       image: '',
-      cost: 'R$:valor,00',
+      cost: 'R$10,00',
     },
     {
       title: 'Item 2',
       quantity: '3',
       image: '',
-      cost: 'R$:valor,00',
+      cost: 'R$10,00',
     },
     {
       title: 'Item 3',
       quantity: '5',
       image: '',
-      cost: 'R$:valor,00',
+      cost: 'R$10,00',
     },
   ];
 
@@ -36,14 +36,17 @@ export default function AddedItens() {
           {showItems ? <Minus size={32}/> : <Plus size={32}/>}
           </button>
         </h1>
-        {showItems && (
+        {showItems ? (
           <ul className={styles.list}>
            { list.map((item) =>
                <CardList title={item.title} quantity={item.quantity} image={item.image} cost={item.cost}/>
             )}
           </ul>
-        )}
+        ):
         <button className={styles.morebutton}><DotsThree size={32}/></button>
+        }
+        
+       
       </div>
   )
 }
