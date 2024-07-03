3113aaef041d3e95619598723a64991fce65de55
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2024-07-03 19:40:22 -0300

    feat: create nav bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,22 @@
-import styles from './styles/Header.module.css'
-import logo from '../../img/MarkeToGo.svg'
-import { Link } from 'react-router-dom'
+import styles from "./styles/Header.module.css";
+import logo from "../../img/MarkeToGo.svg";
+import { MagnifyingGlass } from "@phosphor-icons/react";
 
 export default function Header() {
-    return (
-        <div className={styles.placeholder}>
-            <img src={logo} className={styles.logo} />
-            <Link to="/item">Home</Link>
-        </div>
-    )
+  return (
+    <header className={styles.header}>
+      <div>
+        <img src={logo} className={styles.logo} />
+      </div>
+      <div className={styles.options}>
+        <a>Alimentos</a>
+        <a>Bebidas</a>
+        <a>Limpeza</a>
+      </div>
+      <div className={styles.container}>
+        <MagnifyingGlass size={15} />
+        <input type="text" placeholder="Pesquisar" />
+      </div>
+    </header>
+  );
 }
